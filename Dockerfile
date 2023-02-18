dcf261b106e6557d86b0480a9f9f1cd5910c35bc
0xb1b1 <voxelflee@protonmail.com>
2023-02-18 20:08:11 +0300

    Clean up Docker files and source code

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,24 @@
 FROM python:3.10
 
+# Set working directory
 WORKDIR /code
 
-COPY requirements.txt /code/requirements.txt
-
+# Install system dependencies
 # RUN apt-get install libgeos-c1
 
+# Install Python dependencies
+COPY requirements.txt ./
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-COPY ./app /code/app
-COPY ./sql /code/sql
-COPY main.py /code
-COPY settings.py /code
+# Copy source code
+COPY ./src/app ./
+COPY ./src/sql ./
+COPY ./src/main.py ./
+COPY settings.py ./
 
+# Expose port
+EXPOSE 8000
+
+# Run the app
 CMD ["python", "main.py"]
 #CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- settings.py
+++ settings.py
@@ -1,19 +1,19 @@
-import os
-
+from os import getenv
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 DATABASE_URL = (
-    os.getenv("DATABASE_URL") if os.getenv("DATABASE_URL") else "db:sqlite:///db.sqlite"
+    f"postgresql://${getenv('DB_USER')}:${getenv('DB_PASS')}@{getenv('DB_HOST')}:{getenv('DB_PORT')}/${getenv('DB_NAME')}" if getenv("DB_HOST") else "db:sqlite:///db.sqlite"
 )
+
 ACCESS_TOKEN_EXPIRE_MINUTES = (
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-    if os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    if getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
     else 30
 )
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = getenv("SECRET_KEY")
 if not SECRET_KEY:
-    raise Exception("invalid SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM") if os.getenv("ALGORITHM") else "HS256"
+    raise Exception("Invalid SECRET_KEY in environment")
+ALGORITHM = getenv("ALGORITHM") if getenv("ALGORITHM") else "HS256"
